ec79192d4cb764110330ead8c7a12f1d56c9c480
flunder <larsf2005@gmail.com>
2024-01-11 14:54:34 +0000

    tidying measure hook

--- src/utils/hooks/useComponentSize.ts
+++ src/utils/hooks/useComponentSize.ts
@@ -1,15 +1,15 @@
 import React, { useState, useCallback } from "react";
 
 type Size = { width: number; height: number } | null;
+type ReturnProps = [size: Size, onLayout: (event: any) => void];
 
-export const useComponentSize = (): [
-  size: Size,
-  onLayout: (event: any) => void
-] => {
+export const useComponentSize = (): ReturnProps => {
   const [size, setSize] = useState<Size>(null);
-  const onLayout = useCallback((event) => {
+
+  const onLayout = useCallback((event: any) => {
     const { width, height } = event.nativeEvent.layout;
     setSize({ width, height });
   }, []);
+
   return [size, onLayout];
 };
